050e0dfdadb510d564c73ccf253efca5860a23af
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-10-25 17:11:07 +0530

    updated clerk

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,17 +1,30 @@
 import { Link } from "react-router-dom";
-import { Button } from "./ui/button";
+
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 function Header() {
-    return (
-        <>
-            <nav className="py-4 flex justify-between items-center">
-                <Link to="/">
-                    <img src="/logo.png" className="h-20" alt="Hirrd Logo" />
-                </Link>
-                <Button variant="outline">Login</Button>
-            </nav>
-        </>
-    );
+  return (
+    <>
+      <nav className="py-4 flex justify-between items-center">
+        <Link to="/">
+          <img src="/logo.png" className="h-20" alt="Hirrd Logo" />
+        </Link>
+        {/* <Button variant="outline">Login</Button> */}
+
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </nav>
+    </>
+  );
 }
 
 export default Header;
