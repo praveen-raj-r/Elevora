9a9bab545ad0f74cade9559def051fabd2173aa7
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-10-25 20:05:32 +0530

    added onboarding page

--- src/pages/onboarding.jsx
+++ src/pages/onboarding.jsx
@@ -2,42 +2,53 @@ import { Button } from "@/components/ui/button";
 import { useUser } from "@clerk/clerk-react";
 import { useNavigate } from "react-router-dom";
 import { BarLoader } from "react-spinners";
+import { useEffect } from "react";
 
 function Onboarding() {
   const { user, isLoaded } = useUser();
   const navigate = useNavigate();
 
+  const navigateUser = (currRole) => {
+    navigate(currRole === "recruiter" ? "/post-job" : "/jobs");
+  };
+
   const handleRoleSelection = async (role) => {
-    (await user.update({ unsafeMetadata: { role } }))
-      .createPhoneNumber(() => {
-        navigate(role === "recruiter" ? "/post-job" : "/jobs");
+    await user.update({ unsafeMetadata: { role } })
+      .then(() => {
+        console.log(`Role updated to: ${role}`);
+        navigateUser(role);
       })
       .catch((err) => {
         console.error("Error updating role:", err);
       });
   };
 
+  useEffect(() => {
+    if (user?.unsafeMetadata?.role) {
+      navigateUser(user.unsafeMetadata.role);
+    }
+  }, [user]);
+
   if (!isLoaded) {
     return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
   }
 
   return (
-    <div className="flex flex-col items-center justify-center mt-20">
-      <h2 className="font-extrabold tracking-tighter gradient-title text-7xl sm:text-8xl">
+    <div className="flex flex-col items-center justify-center mt-40">
+      <h2 className="gradient-title font-extrabold text-7xl sm:text-8xl tracking-tighter">
         I am a...
       </h2>
-
-      <div className="grid w-full grid-cols-2 gap-4 mt-16 md:px-40">
+      <div className="mt-16 grid grid-cols-2 gap-4 w-full md:px-40">
         <Button
           variant="blue"
-          className="text-2xl h-36"
+          className="h-36 text-2xl"
           onClick={() => handleRoleSelection("candidate")}
         >
           Candidate
         </Button>
         <Button
           variant="destructive"
-          className="text-2xl h-36"
+          className="h-36 text-2xl"
           onClick={() => handleRoleSelection("recruiter")}
         >
           Recruiter
@@ -45,6 +56,6 @@ function Onboarding() {
       </div>
     </div>
   );
-}
+};
 
 export default Onboarding;
