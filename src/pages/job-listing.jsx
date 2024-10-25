058790620690548019d430f54ffffd137b79b988
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-10-25 20:53:33 +0530

    added job listing

--- src/pages/job-listing.jsx
+++ src/pages/job-listing.jsx
@@ -1,5 +1,166 @@
-function JobListing() {
-  return <div>JobListing</div>;
-}
+import { useEffect, useState } from "react";
+import { useUser } from "@clerk/clerk-react";
+import { State } from "country-state-city";
+import { BarLoader } from "react-spinners";
+import useFetch from "@/hooks/use-fetch";
+
+import JobCard from "@/components/job-card";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { getCompanies } from "@/api/apiCompanies";
+import { getJobs } from "@/api/apiJobs";
+
+const JobListing = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [location, setLocation] = useState("");
+  const [company_id, setCompany_id] = useState("");
+
+  const { isLoaded } = useUser();
+
+  const {
+    // loading: loadingCompanies,
+    data: companies,
+    fn: fnCompanies,
+  } = useFetch(getCompanies);
+
+  const {
+    loading: loadingJobs,
+    data: jobs,
+    fn: fnJobs,
+  } = useFetch(getJobs, {
+    location,
+    company_id,
+    searchQuery,
+  });
+
+  useEffect(() => {
+    if (isLoaded) {
+      fnCompanies();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoaded]);
+
+  useEffect(() => {
+    if (isLoaded) fnJobs();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoaded, location, company_id, searchQuery]);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    let formData = new FormData(e.target);
+
+    const query = formData.get("search-query");
+    if (query) setSearchQuery(query);
+  };
+
+  const clearFilters = () => {
+    setSearchQuery("");
+    setCompany_id("");
+    setLocation("");
+  };
+
+  if (!isLoaded) {
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
+  }
+
+  return (
+    <div className="">
+      <h1 className="pb-8 text-6xl font-extrabold text-center gradient-title sm:text-7xl">
+        Latest Jobs
+      </h1>
+      <form
+        onSubmit={handleSearch}
+        className="flex flex-row items-center w-full gap-2 mb-3 h-14"
+      >
+        <Input
+          type="text"
+          placeholder="Search Jobs by Title.."
+          name="search-query"
+          className="flex-1 h-full px-4 text-md"
+        />
+        <Button type="submit" className="h-full sm:w-28" variant="blue">
+          Search
+        </Button>
+      </form>
+
+      <div className="flex flex-col gap-2 sm:flex-row">
+        <Select value={location} onValueChange={(value) => setLocation(value)}>
+          <SelectTrigger>
+            <SelectValue placeholder="Filter by Location" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              {State.getStatesOfCountry("IN").map(({ name }) => {
+                return (
+                  <SelectItem key={name} value={name}>
+                    {name}
+                  </SelectItem>
+                );
+              })}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+
+        <Select
+          value={company_id}
+          onValueChange={(value) => setCompany_id(value)}
+        >
+          <SelectTrigger>
+            <SelectValue placeholder="Filter by Company" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              {companies?.map(({ name, id }) => {
+                return (
+                  <SelectItem key={name} value={id}>
+                    {name}
+                  </SelectItem>
+                );
+              })}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <Button
+          className="sm:w-1/2"
+          variant="destructive"
+          onClick={clearFilters}
+        >
+          Clear Filters
+        </Button>
+      </div>
+
+      {loadingJobs && (
+        <BarLoader className="mt-4" width={"100%"} color="#36d7b7" />
+      )}
+
+      {loadingJobs === false && (
+        <div className="grid gap-4 mt-8 md:grid-cols-2 lg:grid-cols-3">
+          {jobs?.length ? (
+            jobs.map((job) => {
+              return (
+                <JobCard
+                  key={job.id}
+                  job={job}
+                  savedInit={job?.saved?.length > 0}
+                />
+              );
+            })
+          ) : (
+            <div>No Jobs Found 😢</div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default JobListing;
