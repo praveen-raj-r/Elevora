a53cd5c8b26e915fa0c33a2159a21306e85a58f0
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-10-26 12:46:26 +0530

    added my jobs

--- src/pages/my-jobs.jsx
+++ src/pages/my-jobs.jsx
@@ -1,5 +1,29 @@
-function MyJobs() {
-  return <div>MyJobs</div>;
-}
+import CreatedApplications from "@/components/created-applications";
+import CreatedJobs from "@/components/created-jobs";
+import { useUser } from "@clerk/clerk-react";
+import { BarLoader } from "react-spinners";
+
+const MyJobs = () => {
+  const { user, isLoaded } = useUser();
+
+  if (!isLoaded) {
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
+  }
+
+  return (
+    <div>
+      <h1 className="pb-8 text-5xl font-extrabold text-center gradient-title sm:text-7xl">
+        {user?.unsafeMetadata?.role === "candidate"
+          ? "My Applications"
+          : "My Jobs"}
+      </h1>
+      {user?.unsafeMetadata?.role === "candidate" ? (
+        <CreatedApplications />
+      ) : (
+        <CreatedJobs />
+      )}
+    </div>
+  );
+};
 
 export default MyJobs;
