95a7f77af14e9d988a256019547a54eb9dcd1a85
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-10-26 12:42:49 +0530

    added saved-jobs page

--- src/pages/saved-jobs.jsx
+++ src/pages/saved-jobs.jsx
@@ -1,5 +1,56 @@
-function SavedJobs() {
-  return <div>SavedJobs</div>;
-}
+import { getSavedJobs } from "@/api/apiJobs";
+import JobCard from "@/components/job-card";
+import useFetch from "@/hooks/use-fetch";
+import { useUser } from "@clerk/clerk-react";
+import { useEffect } from "react";
+import { BarLoader } from "react-spinners";
+
+const SavedJobs = () => {
+  const { isLoaded } = useUser();
+
+  const {
+    loading: loadingSavedJobs,
+    data: savedJobs,
+    fn: fnSavedJobs,
+  } = useFetch(getSavedJobs);
+
+  useEffect(() => {
+    if (isLoaded) {
+      fnSavedJobs();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoaded]);
+
+  if (!isLoaded || loadingSavedJobs) {
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
+  }
+
+  return (
+    <div>
+      <h1 className="pb-8 text-6xl font-extrabold text-center gradient-title sm:text-7xl">
+        Saved Jobs
+      </h1>
+
+      {loadingSavedJobs === false && (
+        <div className="grid gap-4 mt-8 md:grid-cols-2 lg:grid-cols-3">
+          {savedJobs?.length ? (
+            savedJobs?.map((saved) => {
+              return (
+                <JobCard
+                  key={saved.id}
+                  job={saved?.job}
+                  onJobAction={fnSavedJobs}
+                  savedInit={true}
+                />
+              );
+            })
+          ) : (
+            <div>No Saved Jobs 👀</div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default SavedJobs;
